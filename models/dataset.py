43f29de04963ad727bc1c15b7e7e36bb0b50f687
DarkGeekMS <mohamedshawky911@gmail.com>
2023-06-28 21:10:28 +0300

    fix camera parameters initialization

--- models/dataset.py
+++ models/dataset.py
@@ -79,8 +79,8 @@ class Dataset:
         self.masks  = torch.from_numpy(self.masks_np.astype(np.float32)).cpu()   # [n_images, H, W, 3]
         self.intrinsics_all = torch.stack(self.intrinsics_all).to(self.device)   # [n_images, 4, 4]
         self.intrinsics_all_inv = torch.inverse(self.intrinsics_all)  # [n_images, 4, 4]
-        self.focal = self.intrinsics_all[0][0, 0]
-        self.camera_center = [self.intrinsics_all[0][0, 2], self.intrinsics_all[0][1, 2]]
+        self.focal = self.intrinsics_all[0][0, 0].cpu()
+        self.camera_center = [self.intrinsics_all[0][0, 2].cpu(), self.intrinsics_all[0][1, 2].cpu()]
         self.pose_all = torch.stack(self.pose_all).to(self.device)  # [n_images, 4, 4]
         self.H, self.W = self.images.shape[1], self.images.shape[2]
         self.image_pixels = self.H * self.W
@@ -88,10 +88,10 @@ class Dataset:
         # Camera Networks
         self.pose_network = LearnPose(
             num_cams=len(self.pose_all), learn_R=True, learn_t=True, init_c2w=self.pose_all
-        )
+        ).to(self.device)
         self.intrinsic_network = LearnFocal(
             H=self.H, W=self.W, req_grad=True, fx_only=False, init_focal=self.focal, init_center=self.camera_center
-        )
+        ).to(self.device)
 
         object_bbox_min = np.array([-1.01, -1.01, -1.01, 1.0])
         object_bbox_max = np.array([ 1.01,  1.01,  1.01, 1.0])
