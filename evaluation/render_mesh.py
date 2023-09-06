a978fa4a2e0c040d10d1b74546b9e385fa348cc5
DarkGeekMS <mohamedshawky911@gmail.com>
2023-09-06 16:31:56 +0300

    add chamfer distance for evaluation

--- evaluation/render_mesh.py
+++ evaluation/render_mesh.py
@@ -1,34 +1,10 @@
 import os
-import cv2
 import bpy
 import argparse
 import numpy as np
 from scipy.spatial.transform import Rotation
 
-
-# This function is borrowed from IDR: https://github.com/lioryariv/idr
-def load_K_Rt_from_P(filename, P=None):
-    if P is None:
-        lines = open(filename).read().splitlines()
-        if len(lines) == 4:
-            lines = lines[1:]
-        lines = [[x[0], x[1], x[2], x[3]] for x in (x.split(" ") for x in lines)]
-        P = np.asarray(lines).astype(np.float32).squeeze()
-
-    out = cv2.decomposeProjectionMatrix(P)
-    K = out[0]
-    R = out[1]
-    t = out[2]
-
-    K = K / K[2, 2]
-    intrinsics = np.eye(4)
-    intrinsics[:3, :3] = K
-
-    pose = np.eye(4, dtype=np.float32)
-    pose[:3, :3] = R.transpose()
-    pose[:3, 3] = (t[:3] / t[3])[:, 0]
-
-    return intrinsics, pose
+from models.dataset import load_K_Rt_from_P
 
 
 def read_camera(camera_path):
